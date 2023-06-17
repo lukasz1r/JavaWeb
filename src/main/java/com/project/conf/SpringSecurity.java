13a88bef8386fb097ac832cb977fb8305ce031c4
Geerafix <geerafix@gmail.com>
2023-06-17 22:08:42 +0200

    minor

--- src/main/java/com/project/conf/SpringSecurity.java
+++ src/main/java/com/project/conf/SpringSecurity.java
@@ -30,7 +30,7 @@ public class SpringSecurity {
                         .requestMatchers("/adminPanel/**").hasAnyRole("ADMIN")
                         .requestMatchers("/editUser/**").hasAnyRole("ADMIN")
                         .requestMatchers("/sharedNotes/**").hasAnyRole("USER", "ADMIN", "FULLUSER")
-                        .requestMatchers("/addCategory/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/addCategory/**").hasAnyRole("FULLUSER", "ADMIN")
                         .requestMatchers("/editNote/**").hasAnyRole("FULLUSER", "ADMIN")
 
                         
